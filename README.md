550a108e2a8e2dde1163962ac6ccd68457f88a21
serefdm14 <serefdm14@gmail.com>
2020-01-22 00:12:53 +0300

    Second

---
+++ README.md
@@ -0,0 +1,5 @@
+Uzaktan eğitim veren bir proje olup; sisteme eğitmenlerin yüklediği kursları kullanıcılar
+satın alarak kurslardan yararlanılması amaçlanmaktadır. Kurumun bünyesinde bulunan eğitmenler
+tarafından ve eğitmen olmak isteyen kişiler başvuru yaparak eğitmen olabileceklerdir. 
+Eğitmenler kurs içeriği olarak videolar ve dokümanlar yükleyerek eğitimlerini gerçekleştirebilirler. 
+ 
---
+++ UZEM.PROJECT.UI.MVC/Controllers/IInstructorController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UZEM.PROJECT.BLL.Abstract;
+using UZEM.PROJECT.UI.MVC.Tools;
+using UZEM_PROJECT.Model;
+
+namespace UZEM.PROJECT.UI.MVC.Controllers
+{
+    public class IInstructorController : Controller
+    {
+        IUserService _userService;
+        IInstructorService _instructorService;
+
+        public IInstructorController(IUserService userService,IInstructorService instructorService)
+        {
+            _userService = userService;
+            _instructorService = instructorService;
+        }
+
+        [HttpGet]
+        public ActionResult EgitmenBasvuru()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult EgitmenBasvuru(InstructorClass ınstructor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitmenBasvuru");
+
+            }
+            _instructorService.Insert(ınstructor);
+
+            return View();
+    
+
+        }
+        [HttpGet]
+        public ActionResult EgitmenEkle()
+        {
+
+            UserClass user = Session["Kullanici"] as UserClass;
+            var userID = user.ID;
+            return View(_userService.Get(userID));
+
+
+        }
+        [HttpPost]
+        public ActionResult EgitmenEkle(InstructorClass ınstructor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitmenEkle");
+
+            }
+            _instructorService.Insert(ınstructor);
+    
+      
+            return RedirectToAction("Index","Home");
+
+
+        }
+        [HttpGet]
+        public ActionResult EgitmenProfili(InstructorClass ınstructor)
+        {
+            UserClass user = Session["Kullanici"] as UserClass;
+
+
+            var i = _instructorService.Get(ınstructor.ID);
+            return View("EgitmenProfili", i);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult EgitmenProfili()
+        {
+
+
+            return View();
+        }
+        public ActionResult KartBilgileri()
+        {
+            return View();
+
+        }
+        public ActionResult Kurs()
+        {
+            return View();
+        
+        }
+
+        public ActionResult Hesap()
+        {
+            return View();
+
+        }
+
+
+    }
+}
